dd4dc168c5503f232937a63b6a551f4012192a79
developerwill <info@wilsongomes.me>
2023-01-02 10:42:51 +0100

    Output categories

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,21 @@
+import {CATEGORIES} from '../data/dummy-data';
+import {FlatList} from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+/*
+* We could use ScrollView also since there aren't many categories and the list isn't dynamic
+*/
+
+function renderCategoryItem(itemData) {
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+}
+
+export default function CategoriesScreen() {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item.id }
+            renderItem={renderCategoryItem}
+        />
+    );
+}
