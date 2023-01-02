26387e0b7b9288907162a00297db775f90fde0fa
developerwill <info@wilsongomes.me>
2023-01-02 11:18:11 +0100

    App background color and StatusBar style

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return (
-    <CategoriesScreen/>
-  );
+    return (
+        <>
+            <StatusBar style={'light'}/>
+            <CategoriesScreen/>
+        </>
+    );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
